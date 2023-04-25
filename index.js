4c4db227f9ade75c2b496e3a62419a6fc2bd1144
williamelliott <ironmaiden229@hotmail.com>
2023-04-25 13:38:46 -0400

    project done

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const reversedWord = word.split('').reverse().join('');
+  if (word === reversedWord) {
+    return true;
+  } else {
+    return false
+  }
 }
 
 /* 
@@ -8,6 +13,11 @@ function isPalindrome(word) {
 
 /*
   Add written explanation of your solution here
+  I looked up a proper format on how to make a function that would make a palindrome return a true or false value. The first step is making a const called reversedWord
+  that takes the word and splits it in half, reverse's it and joins the two words back into one. Then I made an if statement that states that if the word strictly equals 
+  the new reversed word it would return true, other wise I made an else statement that would return false. Additionally I could have added an addictional line of code before
+  my const that is word = word.toLowerCase().replace(/ /g,'') and that would have removed any spaces and made sure all the letters were lower case before it split, reversed, then rejoined
+  the word in order to check for palindrome.
 */
 
 // You can run `node index.js` to view these console logs
